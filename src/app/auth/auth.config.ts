b4c68bdd9a76b313a45566d8008c4bf77135239d
Bastian Basler <bastian.basler2002@gmail.com>
2025-05-21 15:03:22 +0200

    changes for EC2

--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -3,8 +3,8 @@ import { PassedInitialConfig } from 'angular-auth-oidc-client';
 export const authConfig: PassedInitialConfig = {
   config: {
     authority: 'https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_gh9RDotOf',
-    redirectUrl: window.location.origin,
-    postLogoutRedirectUri: window.location.origin,
+    redirectUrl: 'https://bastian-basler.de',
+    postLogoutRedirectUri: 'https://bastian-basler.de',
     clientId: '34fiokko0g853qqd750osf419j',
     scope: 'email openid phone',
     responseType: 'code'
