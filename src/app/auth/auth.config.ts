f6371cc8a24c061ceb7e63c11a0ff1812e39de02
RealLasse <lasse@DESKTOP-NP3TL1M.>
2025-05-30 16:09:03 +0200

    change url to app.lbake.space

--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -3,8 +3,8 @@ import { PassedInitialConfig } from 'angular-auth-oidc-client';
 export const authConfig: PassedInitialConfig = {
   config: {
     authority: 'https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_gh9RDotOf',
-    redirectUrl: 'https://lbake.space',
-    postLogoutRedirectUri: 'https://lbake.space',
+    redirectUrl: 'https://app.lbake.space',
+    postLogoutRedirectUri: 'https://app.lbake.space',
     clientId: '34fiokko0g853qqd750osf419j',
     scope: 'email openid phone',
     responseType: 'code'
